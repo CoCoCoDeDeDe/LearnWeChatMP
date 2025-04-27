150ee681f9b636e2af2889ccabe9b3e08155c5a5
CoCoCoDeDeDe <1328253308@qq.com>
2025-04-27 23:21:56 +0800

    test9: doing product page commodity card, todo advertise swiper

--- test9/project/miniprogram/components/deviceCard/index.js
+++ test9/project/miniprogram/components/deviceCard/index.js
@@ -10,9 +10,9 @@ Component({
       value: {
         id: 'default_id',
         previewImgUrl: '/static/images/previewImg/previewImg_aqaq.png',
-        nickname: 'default_nickname',
-        atRegionName: 'default_atRegionName',
-        atZoneName: 'default_atZoneName'
+        nickname: '默认设备昵称',
+        atRegionName: '默认场所',
+        atZoneName: '默认功能区'
       }
       ,
       required: false,
@@ -24,7 +24,20 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    columnBtnsArr: [
+      {
+        imageSrc: '/static/images/icons/icon_deviceDetail_line_dekBlue@2x.png',
+        bindtap: 'onToDeviceDetail'
+      },
+      {
+        imageSrc: '/static/images/icons/icon_edit_line_darkBlue@2x.png',
+        bindtap: 'onToEditDevice'
+      },
+      {
+        imageSrc: '/static/images/icons/icon_delete_line_darkBlue@2x.png',
+        bindtap: 'onToDeleteDevice'
+      }
+    ]
   },
 
   /**
@@ -32,5 +45,16 @@ Component({
    */
   methods: {
 
+    onToDeviceDetail(e) {
+      console.log("onToDeviceDetail e:", e)
+    },
+
+    onToEditDevice(e) {
+      console.log("onToEditDevice e:", e)
+    },
+
+    onToDeleteDevice(e) {
+      console.log("onToDeleteDevice e:", e)
+    }
   }
 })
--- test9/project/miniprogram/pages/product/index.js
+++ test9/project/miniprogram/pages/product/index.js
@@ -5,7 +5,107 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    deviceProfileList: [
+      {
+        id: 'default_id',
+        previewImgUrl: '/static/images/previewImg/previewImg_aqaq.png',
+        nickname: '默认设备昵称1',
+        atRegionName: '默认场所1',
+        atZoneName: '默认功能区1'
+      },
+      {
+        id: 'default_id',
+        previewImgUrl: '/static/images/previewImg/previewImg_aqaq.png',
+        nickname: '默认设备昵称2',
+        atRegionName: '默认场所2',
+        atZoneName: '默认功能区2'
+      },
+      {
+        id: 'default_id',
+        previewImgUrl: '/static/images/previewImg/previewImg_aqaq.png',
+        nickname: '默认设备昵称3',
+        atRegionName: '默认场所3',
+        atZoneName: '默认功能区3'
+      }
+    ],
+    commodityCardProfileList: [
+      {
+        id: '1',
+        previewImgUrl: '/static/images/previewImg/commodityPreviewImg_111111111.jpg',
+        title: '默认标题默认标题默认标题默认标题默认标题默认标题默认标题默认标题',
+        price: '默认价格',
+        promotionList: [
+          {
+            id: 1,
+            title_short: '促销1'
+          },
+          {
+            id: 2,
+            title_short: '促销2'
+          },
+          {
+            id: 3,
+            title_short: '促销2'
+          },
+          {
+            id: 4,
+            title_short: '促销促销4'
+          },
+          {
+            id: 5,
+            title_short: '促销促销促销c5'
+          }
+        ]
+      },
+      {
+        id: '2',
+        previewImgUrl: '/static/images/previewImg/commodityPreviewImg_111111111.jpg',
+        title: '默认标题默认标题默认标题默认标题',
+        price: '默认价格',
+        promotionList: [
+          {
+            id: 1,
+            title_short: '促销1'
+          }
+        ]
+      },
+      {
+        id: '3',
+        previewImgUrl: '/static/images/previewImg/commodityPreviewImg_111111111.jpg',
+        title: '默认标题默认标题默认标题默认标题',
+        price: '默认价格',
+        promotionList: [
+          {
+            id: 1,
+            title_short: '促销1'
+          }
+        ]
+      },
+      {
+        id: '4',
+        previewImgUrl: '/static/images/previewImg/commodityPreviewImg_111111111.jpg',
+        title: '默认标题默认标题默认标题默认标题默认标题默认标题默认标题默认标题',
+        price: '默认价格',
+        promotionList: [
+          {
+            id: 1,
+            title_short: '促销1'
+          }
+        ]
+      },
+      {
+        id: '5',
+        previewImgUrl: '/static/images/previewImg/commodityPreviewImg_111111111.jpg',
+        title: '默认标题默认标题默认标题默认标题',
+        price: '默认价格',
+        promotionList: [
+          {
+            id: 1,
+            title_short: '促销1'
+          }
+        ]
+      }
+    ]
   },
 
   /**
@@ -15,15 +115,8 @@ Page({
 
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-  
-  onAddDevice(e) {
-
+  onToCommodityDetailPage(e) {
+    console.log("onToCommodityDetailPage e:", e)
   }
 
   

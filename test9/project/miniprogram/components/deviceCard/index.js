4c753952f92d42e6376ac4634e2e25df59b1d7a9
CoCoCoDeDeDe <1328253308@qq.com>
2025-04-28 13:59:01 +0800

    test9: done change rpx to px of page product and mine, todo start coding smartLink page

--- test9/project/miniprogram/components/deviceCard/index.js
+++ test9/project/miniprogram/components/deviceCard/index.js
@@ -9,7 +9,7 @@ Component({
       type: Object,
       value: {
         id: 'default_id',
-        previewImgUrl: '/static/images/previewImg/previewImg_aqaq.png',
+        previewImgUrl: 'https://mp-1b9cd3c8-d666-4006-b791-11d5ce02e1be.cdn.bspapp.com/IoT1/test/previewImg_aqaq.png',
         nickname: '默认设备昵称',
         atRegionName: '默认场所',
         atZoneName: '默认功能区'
--- test9/project/miniprogram/pages/product/index.js
+++ test9/project/miniprogram/pages/product/index.js
@@ -8,30 +8,60 @@ Page({
     deviceProfileList: [
       {
         id: 'default_id',
-        previewImgUrl: '/static/images/previewImg/previewImg_aqaq.png',
+        previewImgUrl: 'https://mp-1b9cd3c8-d666-4006-b791-11d5ce02e1be.cdn.bspapp.com/IoT1/test/previewImg_aqaq.png',
         nickname: '默认设备昵称1',
         atRegionName: '默认场所1',
         atZoneName: '默认功能区1'
       },
       {
         id: 'default_id',
-        previewImgUrl: '/static/images/previewImg/previewImg_aqaq.png',
+        previewImgUrl: 'https://mp-1b9cd3c8-d666-4006-b791-11d5ce02e1be.cdn.bspapp.com/IoT1/test/previewImg_aqaq.png',
         nickname: '默认设备昵称2',
         atRegionName: '默认场所2',
         atZoneName: '默认功能区2'
       },
       {
         id: 'default_id',
-        previewImgUrl: '/static/images/previewImg/previewImg_aqaq.png',
+        previewImgUrl: 'https://mp-1b9cd3c8-d666-4006-b791-11d5ce02e1be.cdn.bspapp.com/IoT1/test/previewImg_aqaq.png',
         nickname: '默认设备昵称3',
         atRegionName: '默认场所3',
         atZoneName: '默认功能区3'
       }
     ],
+    advertisementSwiperItemList: [
+      {
+        imgUrl: 'https://mp-1b9cd3c8-d666-4006-b791-11d5ce02e1be.cdn.bspapp.com/IoT1/test/advertisementImg_1.jpg',
+        navigatePath: ''
+      },
+      {
+        imgUrl: 'https://mp-1b9cd3c8-d666-4006-b791-11d5ce02e1be.cdn.bspapp.com/IoT1/test/advertisementImg_2.jpg',
+        navigatePath: ''
+      },
+      {
+        imgUrl: 'https://mp-1b9cd3c8-d666-4006-b791-11d5ce02e1be.cdn.bspapp.com/IoT1/test/advertisementImg_3.jpg',
+        navigatePath: ''
+      },
+      {
+        imgUrl: 'https://mp-1b9cd3c8-d666-4006-b791-11d5ce02e1be.cdn.bspapp.com/IoT1/test/advertisementImg_4.jpg',
+        navigatePath: ''
+      },
+      {
+        imgUrl: 'https://mp-1b9cd3c8-d666-4006-b791-11d5ce02e1be.cdn.bspapp.com/IoT1/test/advertisementImg_5.jpg',
+        navigatePath: ''
+      },
+      {
+        imgUrl: 'https://mp-1b9cd3c8-d666-4006-b791-11d5ce02e1be.cdn.bspapp.com/IoT1/test/advertisementImg_6.jpg',
+        navigatePath: ''
+      },
+      {
+        imgUrl: 'https://mp-1b9cd3c8-d666-4006-b791-11d5ce02e1be.cdn.bspapp.com/IoT1/test/advertisementImg_7.jpg',
+        navigatePath: ''
+      }
+    ],
     commodityCardProfileList: [
       {
         id: '1',
-        previewImgUrl: '/static/images/previewImg/commodityPreviewImg_111111111.jpg',
+        previewImgUrl: 'https://mp-1b9cd3c8-d666-4006-b791-11d5ce02e1be.cdn.bspapp.com/IoT1/test/commodityPreviewImg_111111111.jpg',
         title: '默认标题默认标题默认标题默认标题默认标题默认标题默认标题默认标题',
         price: '默认价格',
         promotionList: [
@@ -59,7 +89,7 @@ Page({
       },
       {
         id: '2',
-        previewImgUrl: '/static/images/previewImg/commodityPreviewImg_111111111.jpg',
+        previewImgUrl: 'https://mp-1b9cd3c8-d666-4006-b791-11d5ce02e1be.cdn.bspapp.com/IoT1/test/commodityPreviewImg_222.jpg',
         title: '默认标题默认标题默认标题默认标题',
         price: '默认价格',
         promotionList: [
@@ -71,7 +101,7 @@ Page({
       },
       {
         id: '3',
-        previewImgUrl: '/static/images/previewImg/commodityPreviewImg_111111111.jpg',
+        previewImgUrl: 'https://mp-1b9cd3c8-d666-4006-b791-11d5ce02e1be.cdn.bspapp.com/IoT1/test/commodityPreviewImg_222.jpg',
         title: '默认标题默认标题默认标题默认标题',
         price: '默认价格',
         promotionList: [
@@ -83,7 +113,7 @@ Page({
       },
       {
         id: '4',
-        previewImgUrl: '/static/images/previewImg/commodityPreviewImg_111111111.jpg',
+        previewImgUrl: 'https://mp-1b9cd3c8-d666-4006-b791-11d5ce02e1be.cdn.bspapp.com/IoT1/test/commodityPreviewImg_111111111.jpg',
         title: '默认标题默认标题默认标题默认标题默认标题默认标题默认标题默认标题',
         price: '默认价格',
         promotionList: [
@@ -95,7 +125,7 @@ Page({
       },
       {
         id: '5',
-        previewImgUrl: '/static/images/previewImg/commodityPreviewImg_111111111.jpg',
+        previewImgUrl: 'https://mp-1b9cd3c8-d666-4006-b791-11d5ce02e1be.cdn.bspapp.com/IoT1/test/commodityPreviewImg_111111111.jpg',
         title: '默认标题默认标题默认标题默认标题',
         price: '默认价格',
         promotionList: [
@@ -115,8 +145,17 @@ Page({
 
   },
 
+  onAddDevice(e) {
+    console.log("onAddDevice e:", e)
+  },
+
   onToCommodityDetailPage(e) {
     console.log("onToCommodityDetailPage e:", e)
+  },
+
+  onToAdvertisementPage(e) {
+    console.log("onToAdvertisementPage e:", e)
+    console.log("onToAdvertisementPage")
   }
 
   
